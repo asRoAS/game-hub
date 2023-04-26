a2cccafda0ca6af46a8edb2f2e0e436f022dfaa5
AsRoAS <saorsa97@gmail.com>
2023-04-26 23:04:23 +0545

    Build a Nav Bar

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,16 @@
 module.exports = {
   env: { browser: true, es2020: true },
   extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime'
   ],
   parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': 'warn',
