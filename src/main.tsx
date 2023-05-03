4fe679ba360768ec8a6a70e009b5607a4c13c837
AsRoAS <saorsa97@gmail.com>
2023-05-03 20:34:12 +0545

    Setup route using react-router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import theme from './theme';
-import './index.css';
+import router from './routes';
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <RouterProvider router={router} />
         <ReactQueryDevtools />
       </QueryClientProvider>
     </ChakraProvider>
---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,7 @@
+function GameDetailPage() {
+  return (
+    <div>GameDetailPage</div>
+  );
+}
+
+export default GameDetailPage;
