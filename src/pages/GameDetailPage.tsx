c27b722e872aa294a4efba157138578a0219b4e6
AsRoAS <saorsa97@gmail.com>
2023-05-04 17:45:17 +0545

    Build Game Screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -6,6 +6,7 @@ import useGame from '../hooks/useGame';
 import ExpandableText from '../components/ExpandableText';
 import GameAttributes from '../components/GameAttributes';
 import GameTrailer from '../components/GameTrailer';
+import GameScreenshots from '../components/GameScreenshots';
 
 function GameDetailPage() {
   const { slug } = useParams();
@@ -22,6 +23,7 @@ function GameDetailPage() {
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
       <GameTrailer gameId={game.id} />
+      <GameScreenshots gameId={game.id} />
     </>
   );
 }
