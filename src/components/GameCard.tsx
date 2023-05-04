411c2cdfb9853b1e93eeaad248c356183703b4e4
AsRoAS <saorsa97@gmail.com>
2023-05-04 14:09:52 +0545

    Refactor - move entity interfaces

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -6,7 +6,7 @@ import {
   Image,
 } from '@chakra-ui/react';
 import { Link } from 'react-router-dom';
-import { Game } from '../hooks/useGames';
+import { Game } from "../entities/Game";
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
